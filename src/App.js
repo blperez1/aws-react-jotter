d30066e6c5319d905019c34d013455b240f47719
Unknown <blossomperez18@gmail.com>
2021-02-22 19:39:25 -0600

    Add list for jots

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import { Link, NavLink } from "react-router-dom";
+import Routes from './Routes';
+import { withRouter } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAuthenticated: false
+        }
+    }
+    render(){
+        return (
+            <Fragment>
+                <div className="navbar navbar-expand-lg navbar-light bg-light">
+                    <Link to="/" className="navbar-brand" href="#"><h1>Jotter.io</h1></Link>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            {this.state.isAuthenticated ? 
+                                <Fragment>
+                                    <li className="nav-item">
+                                        <NavLink onClick={this.handleLogout}>Logout</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/newjot" className="nav-link">New Jot</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/alljots" className="nav-link">All Jots</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/viewjot" className="nav-link">View Jot</NavLink>
+                                    </li>
+                                </Fragment> : 
+                                <Fragment>
+                                    <li className="nav-item">
+                                        <NavLink to="/login" className="nav-link">Login</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink to="/signup" className="nav-link">Signup</NavLink>
+                                    </li>
+                                </Fragment>
+                            } 
+                        </ul>
+                    </div>                   
+                </div>
+                <Routes/>
+            </Fragment>
+        );
+    }
 }
 
-export default App;
+export default withRouter(App);
